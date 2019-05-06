6506caff218e37f6dfd98f1592dc06c163f2260c
isaacNuflo <isaac.nuflo21@gmail.com>
2019-05-06 00:11:57 -0500

    db

--- cachimbogo/settings.py
+++ cachimbogo/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'cachimbogo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cachimbogo',
+        'USER': 'root',
+        'PASSWORD': 'asd123',
+        'HOST': '35.232.238.17',
+        'PORT': '3306',
     }
 }
 
